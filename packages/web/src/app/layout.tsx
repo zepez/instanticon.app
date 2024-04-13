313a4a3002ee7da6662808505ec2bdfa7c2eccb0
zepez <zepezaueralex@gmail.com>
2024-04-13 14:38:04 -0400

    🔍️ More meta tags, og and twitter images

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -7,9 +7,72 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://instanticon.app"),
   title: "Instant Icon Generator",
   description:
     "Instantly generate SVG or PNG icons and logos for your next project.",
+  category: "technology",
+  generator: "Next.js",
+  applicationName: "Instant Icon Generator",
+  keywords: ["Icon generator", "Logo generator"],
+  authors: [{ name: "Alexander Zepezauer", url: "https://zepez.dev" }],
+  creator: "Alexander Zepezauer",
+  formatDetection: {
+    email: true,
+    address: false,
+    telephone: false,
+  },
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Instant Icon Generator",
+    description:
+      "Instantly generate SVG or PNG icons and logos for your next project.",
+    url: "https://instanticon.app",
+    siteName: "Instant Icon Generator",
+    images: [
+      {
+        url: "https://instanticon.app/og.png",
+        width: 800,
+        height: 600,
+        alt: "Instant Icon Generator Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Instant Icon Generator",
+    description:
+      "Instantly generate SVG or PNG icons and logos for your next project.",
+    creator: "@zepezAlex",
+    images: [
+      {
+        url: "https://instanticon.app/twitter.png",
+        alt: "Instant Icon Generator Twitter Image",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  verification: {
+    other: {
+      creator: ["https://zepez.dev", "https://github.com/zepez"],
+    },
+  },
 };
 
 export default function Layout({
